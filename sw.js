9b5c70ea522609ce5c55eb8c21c300ddf76d9eab
notanawat99 <161730208+notanawat99@users.noreply.github.com>
2026-04-09 15:17:55 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,5 @@
-const CACHE_NAME = 'dct-v63';
-const urlsToCache = [
-  '/diamond-copper-liff/',
-  '/diamond-copper-liff/index.html',
-  '/diamond-copper-liff/manifest.json',
-  '/diamond-copper-liff/icon-192.png',
-  '/diamond-copper-liff/icon-512.png'
-];
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).then(res => {
-      if(res && res.status === 200 && e.request.method === 'GET'){
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
-      }
-      return res;
-    }).catch(() => caches.match(e.request))
-  );
-});
+const CACHE_NAME = 'dct-v64';
+const urlsToCache = ['/diamond-copper-liff/','/diamond-copper-liff/index.html','/diamond-copper-liff/manifest.json','/diamond-copper-liff/icon-192.png','/diamond-copper-liff/icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(res=>{if(res&&res.status===200&&e.request.method==='GET'){const clone=res.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,clone));}return res;}).catch(()=>caches.match(e.request)));});
